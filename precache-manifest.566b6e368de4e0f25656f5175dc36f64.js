9e552143c02ee70bbc02955bc8719a8fc61d3bac
Simon Renault <simon@omegaanalyticsystems.co.uk>
2019-02-04 16:45:52 +0000

    Initial commit
    Tested locally on web
    Partilly responsive

---
+++ precache-manifest.566b6e368de4e0f25656f5175dc36f64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3927db10fcbb7b7248c3",
+    "url": "/static/js/main.3927db10.chunk.js"
+  },
+  {
+    "revision": "741151eae6b9f6a29761",
+    "url": "/static/js/1.741151ea.chunk.js"
+  },
+  {
+    "revision": "3927db10fcbb7b7248c3",
+    "url": "/static/css/main.54ada8f2.chunk.css"
+  },
+  {
+    "revision": "c433afec25040964ee5f18d40db76332",
+    "url": "/index.html"
+  }
+];
